fe3bc9367260d25f5c3b37ccd5b512086365ecc5
Luyanda Siko <sikoluyanda@gmail.com>
2020-11-18 00:57:21 +0200

    Updated EmailAddress Value-object to mark it as PrimaryContact

--- src/Contracts/EmailAddress.php
+++ src/Contracts/EmailAddress.php
@@ -6,7 +6,10 @@ namespace DigitalClosuxe\Business\Profile\Contracts;
  * Class EmailAddress
  * 
  * Email addresses are treated as Value-Objects
+ * 
+ * NB: Within the Business Context EmailAddress Value-Objects 
+ * defualt to primary as means to  reachout to Profile Owners
  */
-interface EmailAddress
+interface EmailAddress extends PrimaryContact
 {
 }
