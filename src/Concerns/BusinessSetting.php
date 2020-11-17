cf6834e3b52cebe168d4d85cfb5ec76080451637
Luyanda Siko <sikoluyanda@gmail.com>
2020-11-18 01:32:48 +0200

    Added new busniess concerns around Business Profiles

---
+++ src/Concerns/BusinessSetting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace DigitalClosuxe\Business\Profile\Concerns;
+
+trait BusinessSetting
+{
+   /** [@inheritdoc] */
+   public function hasSettings(): bool
+   {
+       return false;
+   } 
+}
