6ecdc8bb36e3522cf3a93a60342fa28986a4bebd
Luyanda Siko <sikoluyanda@gmail.com>
2020-11-19 20:14:16 +0200

    Added Account and Contact models for the Profile AggregateRoot

---
+++ tests/fixtures/models/Account.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    public $timestamps = true;
+}
---
+++ tests/fixtures/models/Contact.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    public $timestamps = true;
+}
