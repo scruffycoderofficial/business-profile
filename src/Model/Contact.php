52bdaa7825d901384a300702fa882f737706f6e6
Luyanda Siko <sikoluyanda@gmail.com>
2020-11-25 23:42:31 +0200

    Created internal related object linking to the ghost Profile AR

--- src/Model/Contact.php
+++ src/Model/Contact.php
@@ -3,9 +3,32 @@
 namespace DigitalClosuxe\Business\Profile\Model;
 
 use DigitalClosuxe\Business\Profile\Contracts\ContactProfile;
+use DigitalClosuxe\Business\Profile\Contracts\ { EmailAddress, MobileNumber };
 use DigitalClosuxe\Business\Profile\Concerns\{ Profile, Contact as ContactConcern };
 
 class Contact implements ContactProfile
 {
     use Profile, ContactConcern;
+
+    public function contactNumber(): MobileNumber
+    {
+        return new class implements MobileNumber
+        {
+            public function contactNumber(): MobileNumber
+            {
+                return new self();
+            }
+        };
+    }
+
+    public function contactEmail(): EmailAddress
+    {
+        return new class implements EmailAddress
+        {
+            public function contactEmail(): EmailAddress
+            {
+                return new self();
+            }
+        };
+    }
 }
--- src/Model/Profile.php
+++ src/Model/Profile.php
@@ -2,6 +2,7 @@
 
 namespace DigitalClosuxe\Business\Profile\Model;
 
+use DigitalClosuxe\Business\Profile\Contracts\ { MobileNumber, EmailAddress };
 use DigitalClosuxe\Business\Profile\Contracts\{ AccountProfile, ContactProfile };
 use DigitalClosuxe\Business\Profile\Concerns\{ Profile as ProfileConcern, Contact, Setting };
 
@@ -12,9 +13,36 @@ class Profile
 {
     public function getContact()
     {
-        return new class implements ContactProfile
+        return new class implements ContactProfile, AccountProfile
         {
             use ProfileConcern, Contact;
+
+            public function contactNumber(): MobileNumber
+            {
+                return new class implements MobileNumber
+                {
+                    public function contactNumber(): MobileNumber
+                    {
+                        return new self();
+                    }
+                };
+            }
+
+            public function contactEmail(): EmailAddress
+            {
+                return new class implements EmailAddress
+                {
+                    public function contactEmail(): EmailAddress
+                    {
+                        return new self();
+                    }
+                };
+            }
+
+            public function hasSettings(): bool
+            {
+                return false;
+            }
         };
     }
 
@@ -24,5 +52,10 @@ class Profile
         {
             use ProfileConcern, Setting;
         };
+    }
+
+    public function hasSettings(): bool
+    {
+        return false;
     }
 }
