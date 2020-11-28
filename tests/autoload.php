0a45141b7cc61a4eb3ba0d883e6faed3c818776c
Luyanda Siko <sikoluyanda@gmail.com>
2020-11-29 00:16:43 +0200

    Cleaned up tests autoload file

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,17 +1,3 @@
 <?php
 
-define(
-    'MODULE_NAMESPACE', 
-    'DigitalClosuxe\\Business\\Extension\\Eloquent\\Models\\'
-);
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
-//instatiate a new ClassLoader instance
-$loader = new \Composer\Autoload\ClassLoader();
-
-// register classes with namespaces
-$loader->addPsr4(MODULE_NAMESPACE, __DIR__ . '/fixtures/extensions/eloquent/models');
-
-// activate the autoloader
-$loader->register();
